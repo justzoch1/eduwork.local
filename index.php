b80420bb3e636925d893a893b571c7a8015a6054
Павел Виктор <146018786+justzoch1@users.noreply.github.com>
2024-08-15 12:02:23 +0300

    merging with a branch Three

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $news = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <?php foreach ($news as $new): ?>
             <div class="col-md-4 mb-4">
                 <div class="card">
-                    <img src="assets/images/two.gif" class="card-img-top" alt="<?php echo $new['name']; ?>">
+                    <img src="assets/images/News.gif" class="card-img-top" alt="<?php echo $new['name']; ?>">
                     <div class="card-body">
                         <h5 class="card-title"><?php echo $new['name']; ?></h5>
                         <p class="card-text"><?php echo $new['description']; ?></p>
